3f9184806c550d2574760ffe849e3a6590ae02a5
Brian Campbell <Brian.Campbell@ed.ac.uk>
2024-10-16 17:22:23 +0100

    Rearrange monad definition
    Avoids silly eta-expansions that get in the way of typeclass resolution
    and type checking.

--- src/ConcurrencyInterfaceBuiltins.v
+++ src/ConcurrencyInterfaceBuiltins.v
@@ -11,31 +11,31 @@ Open Scope Z.
 
 Module Defs (A : Arch) (I : InterfaceT A).
 
-Definition monad A E := I.iMon (fun _ => E) A.
-Definition returnm {A E} : A -> monad A E := I.Ret.
+Definition monad E := I.iMon (fun _ => E).
+Definition returnm {A E} : A -> monad E A := I.Ret.
 
-Definition monadR A R E := I.iMon (fun _ => (R + E)%type) A.
-Definition returnR {A E} R : A -> monadR A R E := I.Ret.
+Definition monadR R E := I.iMon (fun _ => (R + E)%type).
+Definition returnR {A E} R : A -> monadR R E A := I.Ret.
 
-Definition bind {A B E : Type} (m : monad A E) (f : A -> monad B E) : monad B E := I.iMon_bind m f.
+Definition bind {A B E : Type} (m : monad E A) (f : A -> monad E B) : monad E B := I.iMon_bind m f.
 Notation "m >>= f" := (bind m f) (at level 50, left associativity).
-Definition bind0 {A E} (m : monad unit E) (n : monad A E) :=
+Definition bind0 {A E} (m : monad E unit) (n : monad E A) :=
   m >>= fun (_ : unit) => n.
 Notation "m >> n" := (bind0 m n) (at level 50, left associativity).
 
-Definition fail {A E} (msg : string) : monad A E :=
+Definition fail {A E} (msg : string) : monad E A :=
   I.Next (I.GenericFail msg) (fun f => match f with end).
 
-Definition exit {A E} (_ : unit) : monad A E := fail "exit".
+Definition exit {A E} (_ : unit) : monad E A := fail "exit".
 
-Definition choose_bool {E} (_descr : string) : monad bool E := I.Next (I.Choose ChooseBool) mret.
-Definition choose_bit {E} (_descr : string) : monad bitU E := I.Next (I.Choose ChooseBit) mret.
-Definition choose_int {E} (_descr : string) : monad Z E := I.Next (I.Choose ChooseInt) mret.
-Definition choose_nat {E} (_descr : string) : monad Z E := I.Next (I.Choose ChooseNat) mret.
-Definition choose_real {E} (_descr : string) : monad R E := I.Next (I.Choose ChooseReal) mret.
-Definition choose_string {E} (_descr : string) : monad string E := I.Next (I.Choose ChooseString) mret.
-Definition choose_range {E} (_descr : string) (lo hi : Z) : monad Z E := I.Next (I.Choose (ChooseRange lo hi)) mret.
-Definition choose_bitvector {E} (_descr : string) (n : Z) : monad (mword n) E :=
+Definition choose_bool {E} (_descr : string) : monad E bool := I.Next (I.Choose ChooseBool) mret.
+Definition choose_bit {E} (_descr : string) : monad E bitU := I.Next (I.Choose ChooseBit) mret.
+Definition choose_int {E} (_descr : string) : monad E Z := I.Next (I.Choose ChooseInt) mret.
+Definition choose_nat {E} (_descr : string) : monad E Z := I.Next (I.Choose ChooseNat) mret.
+Definition choose_real {E} (_descr : string) : monad E R := I.Next (I.Choose ChooseReal) mret.
+Definition choose_string {E} (_descr : string) : monad E string := I.Next (I.Choose ChooseString) mret.
+Definition choose_range {E} (_descr : string) (lo hi : Z) : monad E Z := I.Next (I.Choose (ChooseRange lo hi)) mret.
+Definition choose_bitvector {E} (_descr : string) (n : Z) : monad E (mword n) :=
   I.Next (I.Choose (ChooseBitvector n)) mret.
 (* match n return monad (mword n) E with
  | Zneg _ => returnm (bv_0 0)
@@ -43,14 +43,14 @@ Definition choose_bitvector {E} (_descr : string) (n : Z) : monad (mword n) E :=
  | Zpos p => I.Next (I.Choose (N.of_nat (Pos.to_nat p))) (fun bv => returnm bv)
  end.*)
 
-Definition assert_exp {E} (exp :bool) msg : monad unit E :=
+Definition assert_exp {E} (exp :bool) msg : monad E unit :=
  if exp then returnm tt else fail msg.
-Definition assert_exp' {E} (exp :bool) msg : monad (exp = true) E :=
- if exp return monad (exp = true) E then returnm eq_refl else fail msg.
+Definition assert_exp' {E} (exp :bool) msg : monad E (exp = true) :=
+ if exp return monad E (exp = true) then returnm eq_refl else fail msg.
 
-Definition throw {A E} (e : E) : monad A E := I.Next (I.ExtraOutcome e) mret.
+Definition throw {A E} (e : E) : monad E A := I.Next (I.ExtraOutcome e) mret.
 
-Fixpoint try_catch {A E1 E2} (m : monad A E1) (h : E1 -> monad A E2) : monad A E2 :=
+Fixpoint try_catch {A E1 E2} (m : monad E1 A) (h : E1 -> monad E2 A) : monad E2 A :=
   match m with
   | I.Ret r => I.Ret r
   | I.Next (I.ExtraOutcome e)                  _ => h e
@@ -72,8 +72,8 @@ Fixpoint try_catch {A E1 E2} (m : monad A E1) (h : E1 -> monad A E2) : monad A E
   | I.Next (I.Discard)                         f => I.Next (I.Discard) (fun t => try_catch (f t) h)
   end.
 
-Definition early_return {A R E} (r : R) : monadR A R E := throw (inl r).
-Definition catch_early_return {A E} (m : monadR A A E) : monad A E :=
+Definition early_return {A R E} (r : R) : monadR R E A := throw (inl r).
+Definition catch_early_return {A E} (m : monadR A E A) : monad E A :=
   try_catch m
     (fun r => match r with
       | inl a => returnm a
@@ -96,11 +96,11 @@ Definition pure_early_return {A} (v : A + A) : A :=
   end.
 
 (* Lift to monad with early return by wrapping exceptions *)
-Definition liftR {A R E} (m : monad A E) : monadR A R E :=
+Definition liftR {A R E} (m : monad E A) : monadR R E A :=
  try_catch m (fun e => throw (inr e)).
 
 (* Catch exceptions in the presence : early returns *)
-Definition try_catchR {A R E} (m : monadR A R E) (h : E -> monadR A R E) :=
+Definition try_catchR {A R E} (m : monadR R E A) (h : E -> monadR R E A) :=
   try_catch m
     (fun r => match r with
       | inl r => throw (inl r)
@@ -111,41 +111,41 @@ Definition try_catchR {A R E} (m : monadR A R E) (h : E -> monadR A R E) :=
 
 Section Undef.
 
-Definition undefined_unit {E} (_:unit) : monad unit E := returnm tt.
-Definition undefined_bool {E} (_:unit) : monad bool E := choose_bool "undefined_bool".
-Definition undefined_bit {E} (_:unit) : monad bitU E := choose_bool "undefined_bit" >>= fun b => returnm (bitU_of_bool b).
-Definition undefined_string {E} (_:unit) : monad string E := choose_string "undefined_string".
- Definition undefined_int {E} (_:unit) : monad Z E := choose_int "undefined_int".
-Definition undefined_nat {E} (_:unit) : monad Z E := choose_nat "undefined_nat".
-Definition undefined_real {E} (_:unit) : monad _ E := choose_real "undefined_real".
-Definition undefined_range {E} i j : monad Z E := choose_range "undefined_range" i j.
-Definition undefined_bitvector {E} n : monad (mword n) E := choose_bitvector "undefined_bitvector" n.
+Definition undefined_unit {E} (_:unit) : monad E unit := returnm tt.
+Definition undefined_bool {E} (_:unit) : monad E bool := choose_bool "undefined_bool".
+Definition undefined_bit {E} (_:unit) : monad E bitU := choose_bool "undefined_bit" >>= fun b => returnm (bitU_of_bool b).
+Definition undefined_string {E} (_:unit) : monad E string := choose_string "undefined_string".
+ Definition undefined_int {E} (_:unit) : monad E Z := choose_int "undefined_int".
+Definition undefined_nat {E} (_:unit) : monad E Z := choose_nat "undefined_nat".
+Definition undefined_real {E} (_:unit) : monad E _ := choose_real "undefined_real".
+Definition undefined_range {E} i j : monad E Z := choose_range "undefined_range" i j.
+Definition undefined_bitvector {E} n : monad E (mword n) := choose_bitvector "undefined_bitvector" n.
 
-Definition undefined_vector {E T} n `{Inhabited T} (a:T) : monad (vec T n) E := returnm (vector_init n a).
+Definition undefined_vector {E T} n `{Inhabited T} (a:T) : monad E (vec T n) := returnm (vector_init n a).
 
 End Undef.
 
 (* ---- Prompt_monad *)
 
-Definition read_reg {a e} (reg : A.reg a) : monad a e :=
+Definition read_reg {a e} (reg : A.reg a) : monad e a :=
   let k v := I.Ret v in
   I.Next (I.RegRead reg None) k.
 
-Definition read_reg_ref {a e} (ref : register_ref A.reg a) : monad a e :=
+Definition read_reg_ref {a e} (ref : register_ref A.reg a) : monad e a :=
   let k v := I.Ret v in
   I.Next (I.RegRead ref.(Values.reg) None) k.
 
 Definition reg_deref {a e} := @read_reg_ref a e.
 
-Definition write_reg {a e} (reg : A.reg a) (v : a) : monad unit e :=
+Definition write_reg {a e} (reg : A.reg a) (v : a) : monad e unit :=
  I.Next (I.RegWrite reg None v) I.Ret.
 
-Definition write_reg_ref {a e} (ref : register_ref A.reg a) (v : a) : monad unit e :=
+Definition write_reg_ref {a e} (ref : register_ref A.reg a) (v : a) : monad e unit :=
  I.Next (I.RegWrite ref.(Values.reg) None v) I.Ret.
 
 (* ---- Prompt *)
 
-Fixpoint foreachM {a e Vars} (l : list a) (vars : Vars) (body : a -> Vars -> monad Vars e) : monad Vars e :=
+Fixpoint foreachM {a e Vars} (l : list a) (vars : Vars) (body : a -> Vars -> monad e Vars) : monad e Vars :=
 match l with
 | [] => returnm vars
 | (x :: xs) =>
@@ -161,7 +161,7 @@ match l with
   foreachE xs vars body
 end.
 
-Fixpoint foreach_ZM_up' {E Vars} (from to step off : Z) (n : nat) (* 0 <? step *) (* 0 <=? off *) (vars : Vars) (body : forall (z : Z) (* from <=? z <=? to *), Vars -> monad Vars E) {struct n} : monad Vars E :=
+Fixpoint foreach_ZM_up' {E Vars} (from to step off : Z) (n : nat) (* 0 <? step *) (* 0 <=? off *) (vars : Vars) (body : forall (z : Z) (* from <=? z <=? to *), Vars -> monad E Vars) {struct n} : monad E Vars :=
   if sumbool_of_bool (from + off <=? to) then
     match n with
     | O => returnm vars
@@ -177,7 +177,7 @@ Fixpoint foreach_ZE_up' {e Vars} (from to step off : Z) (n : nat) (* 0 <? step *
     end
   else inr vars.
 
-Fixpoint foreach_ZM_down' {E Vars} (from to step off : Z) (n : nat) (* 0 <? step *) (* off <=? 0 *) (vars : Vars) (body : forall (z : Z) (* to <=? z <=? from *), Vars -> monad Vars E) {struct n} : monad Vars E :=
+Fixpoint foreach_ZM_down' {E Vars} (from to step off : Z) (n : nat) (* 0 <? step *) (* off <=? 0 *) (vars : Vars) (body : forall (z : Z) (* to <=? z <=? from *), Vars -> monad E Vars) {struct n} : monad E Vars :=
   if sumbool_of_bool (to <=? from + off) then
     match n with
     | O => returnm vars
@@ -203,14 +203,14 @@ Definition foreach_ZE_up {e Vars} from to step vars body (* 0 <? step *) :=
 Definition foreach_ZE_down {e Vars} from to step vars body (* 0 <? step *) :=
     foreach_ZE_down' (e := e) (Vars := Vars) from to step 0 (S (Z.abs_nat (from - to))) vars body.
 
-Definition genlistM {A E} (f : nat -> monad A E) (n : nat) : monad (list A) E :=
+Definition genlistM {A E} (f : nat -> monad E A) (n : nat) : monad E (list A) :=
   let indices := List.seq 0 n in
   foreachM indices [] (fun n xs => (f n >>= (fun x => returnm (xs ++ [x])%list))).
 
-Definition and_boolM {E} (l : monad bool E) (r : monad bool E) : monad bool E :=
+Definition and_boolM {E} (l : monad E bool) (r : monad E bool) : monad E bool :=
  l >>= (fun l => if l then r else returnm false).
 
-Definition or_boolM {E} (l : monad bool E) (r : monad bool E) : monad bool E :=
+Definition or_boolM {E} (l : monad E bool) (r : monad E bool) : monad E bool :=
  l >>= (fun l => if l then returnm true else r).
 
 
@@ -245,7 +245,7 @@ Definition Zwf_guarded (z:Z) : Acc (Zwf 0) z :=
 
 (*val whileM : forall 'rv 'vars 'e. 'vars -> ('vars -> monad 'rv bool 'e) ->
                 ('vars -> monad 'rv 'vars 'e) -> monad 'rv 'vars 'e*)
-Fixpoint whileMT' {E Vars} limit (vars : Vars) (cond : Vars -> monad bool E) (body : Vars -> monad Vars E) (acc : Acc (Zwf 0) limit) : monad Vars E.
+Fixpoint whileMT' {E Vars} limit (vars : Vars) (cond : Vars -> monad E bool) (body : Vars -> monad E Vars) (acc : Acc (Zwf 0) limit) : monad E Vars.
 exact (
   if Z_ge_dec limit 0 then
     cond vars >>= fun cond_val =>
@@ -255,11 +255,11 @@ exact (
   else fail "Termination limit reached").
 Defined.
 
-Definition whileMT {E Vars} (vars : Vars) (measure : Vars -> Z) (cond : Vars -> monad bool E) (body : Vars -> monad Vars E) : monad Vars E :=
+Definition whileMT {E Vars} (vars : Vars) (measure : Vars -> Z) (cond : Vars -> monad E bool) (body : Vars -> monad E Vars) : monad E Vars :=
   let limit := measure vars in
   whileMT' limit vars cond body (Zwf_guarded limit).
 
-Fixpoint untilMT' {E Vars} limit (vars : Vars) (cond : Vars -> monad bool E) (body : Vars -> monad Vars E) (acc : Acc (Zwf 0) limit) : monad Vars E.
+Fixpoint untilMT' {E Vars} limit (vars : Vars) (cond : Vars -> monad E bool) (body : Vars -> monad E Vars) (acc : Acc (Zwf 0) limit) : monad E Vars.
 exact (
   if Z_ge_dec limit 0 then
     body vars >>= fun vars =>
@@ -268,13 +268,13 @@ exact (
   else fail "Termination limit reached").
 Defined.
 
-Definition untilMT {E Vars} (vars : Vars) (measure : Vars -> Z) (cond : Vars -> monad bool E) (body : Vars -> monad Vars E) : monad Vars E :=
+Definition untilMT {E Vars} (vars : Vars) (measure : Vars -> Z) (cond : Vars -> monad E bool) (body : Vars -> monad E Vars) : monad E Vars :=
   let limit := measure vars in
   untilMT' limit vars cond body (Zwf_guarded limit).
 
 Section Choose.
 
-Definition choose_from_list {A E} (descr : string) (xs : list A) : monad A E :=
+Definition choose_from_list {A E} (descr : string) (xs : list A) : monad E A :=
   (* Use sufficiently many nondeterministically chosen bits and convert into an
      index into the list *)
   choose_range descr 0 (Z.of_nat (List.length xs) - 1) >>= fun idx =>
@@ -283,42 +283,42 @@ Definition choose_from_list {A E} (descr : string) (xs : list A) : monad A E :=
     | None => fail ("choose " ++ descr)
   end.
 
-Definition internal_pick {a e} (xs : list a) : monad a e :=
+Definition internal_pick {a e} (xs : list a) : monad e a :=
   choose_from_list "internal_pick" xs.
 
 End Choose.
 
 (* --- Operators_mwords.v *)
 
-Definition autocast_m {e m n} {T : Z -> Type} `{H : Inhabited (T n)} (x : monad (T m) e) : monad (T n) e :=
+Definition autocast_m {e m n} {T : Z -> Type} `{H : Inhabited (T n)} (x : monad e (T m)) : monad e (T n) :=
   x >>= fun x => returnm (autocast x).
 
 (* --- should probably define generic versions of these, parametrised by Arch *)
 
-Definition sail_barrier {e} (b : A.barrier) : monad unit e :=
+Definition sail_barrier {e} (b : A.barrier) : monad e unit :=
   I.Next (I.Barrier b) I.Ret.
 
-Definition sail_take_exception {e} (f : A.fault) : monad unit e :=
+Definition sail_take_exception {e} (f : A.fault) : monad e unit :=
   I.Next (I.TakeException f) I.Ret.
 
-Definition sail_return_exception {e} pa : monad unit e :=
+Definition sail_return_exception {e} pa : monad e unit :=
   I.Next (I.ReturnException pa) I.Ret.
 
-Definition sail_cache_op {e} (op : A.cache_op) : monad unit e :=
+Definition sail_cache_op {e} (op : A.cache_op) : monad e unit :=
   I.Next (I.CacheOp op) I.Ret.
 
-Definition sail_tlbi {e} (op : A.tlb_op) : monad unit e :=
+Definition sail_tlbi {e} (op : A.tlb_op) : monad e unit :=
   I.Next (I.TlbOp op) I.Ret.
 
-Definition branch_announce {e} sz (addr : mword sz) : monad unit e :=
+Definition branch_announce {e} sz (addr : mword sz) : monad e unit :=
   (* TODO: branch_announce seems rather odd *)
   I.Next (I.BranchAnnounce sz addr) I.Ret.
 
-Definition instr_announce {e sz} (opcode : mword sz) : monad unit e :=
+Definition instr_announce {e sz} (opcode : mword sz) : monad e unit :=
   I.Next (I.InstrAnnounce (bv_to_bvn (get_word opcode))) I.Ret.
 
-Definition cycle_count {e} (_ : unit) : monad unit e := I.Next I.CycleCount I.Ret.
-Definition get_cycle_count {e} (_ : unit) : monad Z e := I.Next I.GetCycleCount I.Ret.
+Definition cycle_count {e} (_ : unit) : monad e unit := I.Next I.CycleCount I.Ret.
+Definition get_cycle_count {e} (_ : unit) : monad e Z := I.Next I.GetCycleCount I.Ret.
 
 Definition cast_N  {T : N -> Type} {m n} : forall (x : T m) (eq : m = n), T n.
 refine (match m,n with
@@ -328,7 +328,7 @@ refine (match m,n with
 end); congruence.
 Defined.
 
-Definition sail_mem_read {e n} (req : Mem_read_request n (Z.of_N A.va_size) A.pa A.translation A.arch_ak) : monad (result (mword (8 * n) * option bool) A.abort) e.
+Definition sail_mem_read {e n} (req : Mem_read_request n (Z.of_N A.va_size) A.pa A.translation A.arch_ak) : monad e (result (mword (8 * n) * option bool) A.abort).
 refine (
   let n' := Z.to_N n in
   let va : option (bv A.va_size) :=
@@ -357,7 +357,7 @@ Unshelve.
 reflexivity.
 Defined.
 
-Definition sail_mem_write {e n} (req : Mem_write_request n (Z.of_N A.va_size) A.pa A.translation A.arch_ak) : monad (result (option bool) A.abort) e.
+Definition sail_mem_write {e n} (req : Mem_write_request n (Z.of_N A.va_size) A.pa A.translation A.arch_ak) : monad e (result (option bool) A.abort).
 refine (
   let n' := Z.to_N n in
   match req.(Mem_write_request_value) with
@@ -400,10 +400,10 @@ Unshelve.
 reflexivity.
 Defined.
 
-Definition sail_sys_reg_read {e T} (id : A.sys_reg_id) (r : register_ref A.reg T) : monad T e :=
+Definition sail_sys_reg_read {e T} (id : A.sys_reg_id) (r : register_ref A.reg T) : monad e T :=
   I.Next (I.RegRead r.(Values.reg) (Some id)) I.Ret.
 
-Definition sail_sys_reg_write {e T} (id : A.sys_reg_id) (r : register_ref A.reg T) (v : T) : monad unit e :=
+Definition sail_sys_reg_write {e T} (id : A.sys_reg_id) (r : register_ref A.reg T) (v : T) : monad e unit :=
   I.Next (I.RegWrite r.(Values.reg) (Some id) v) I.Ret.
 
 (* ----------- *)
